93ec1fe73a01c20e1d4afc263ebe8d74c169893a
PeshkaAbramovoi <63866529+PeshkaAbramovoi@users.noreply.github.com>
2020-11-28 10:17:43 +0400

    Moved main menu render to UI class

--- FrescoQuestions/UI.cs
+++ FrescoQuestions/UI.cs
@@ -11,7 +11,7 @@ namespace FrescoQuestions
 {
     public static class UI
     {
-        private static Form1 FormHandler { get; set; }
+        private static Form FormHandler { get; set; }
         private static int FormWidth => FormHandler.Width;
         private static int FormHeight => FormHandler.Height;
         private static Control.ControlCollection Controls => FormHandler.Controls;
@@ -19,7 +19,7 @@ namespace FrescoQuestions
         public static QuestionPath QuestionPath { get; private set; }
         private static bool Initialized { get; set; } = false;
         public static bool IsTestFinished { get; private set; } = false;
-        public static void Initialize(Form1 formHandler)
+        public static void Initialize<T>(T formHandler) where T : Form
         {
             if (formHandler == null) throw new NullReferenceException();
             
@@ -27,8 +27,9 @@ namespace FrescoQuestions
             {
                 FormHandler = formHandler;
                 FormHandler.Text = Properties.Resources.AppName;
-                FormHandler.exitMenuItem.Click += (s, e) => { Application.Exit(); };
-                FormHandler.aboutMenuItem.Click += (s, e) => { (new AboutForm()).ShowDialog(); };
+                Components.ExitMenuItem.Click += (s, e) => { Application.Exit(); };
+                Components.AboutMenuItem.Click += (s, e) => { (new AboutForm()).ShowDialog(); };
+                Renderer.Menu.Render();
 
                 Initialized = true;
             }
@@ -82,6 +83,15 @@ namespace FrescoQuestions
             Renderer.Test.Render(QuestionPath.CurrentQuestion);
             CreateButtonsEvents();
         }
+        public static class Components
+        {
+            public static ToolStripMenuItem TestMenuItem { get; private set; } = new ToolStripMenuItem();
+            public static ToolStripMenuItem HelpMenuItem { get; private set; } = new ToolStripMenuItem();
+            public static ToolStripMenuItem StartTestMenuItem { get; private set; } = new ToolStripMenuItem();
+            public static ToolStripMenuItem ExitMenuItem { get; private set; } = new ToolStripMenuItem();
+            public static ToolStripMenuItem AboutMenuItem { get; private set; } = new ToolStripMenuItem();
+            public static MenuStrip MainMenu { get; private set; } = new MenuStrip();
+        }
         private static class Renderer
         {
             public static class Test
@@ -89,32 +99,94 @@ namespace FrescoQuestions
                 public static Button LeftButton { get; private set; } = new Button();
                 public static Button RightButton { get; private set; } = new Button();
                 public static Label QuestionLabel { get; private set; } = new Label();
+                public static Control.ControlCollection ControlCollection { get; private set; } = new Control.ControlCollection(new Control());
                 public static void InitializeComponents(Question question)
                 {
                     /* Question label properties */
                     QuestionLabel.Text = question.QuestionText;
                     QuestionLabel.Dock = DockStyle.Fill;
                     QuestionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-                    QuestionLabel.Font = new System.Drawing.Font("Calibri Light", 24); ;
+                    QuestionLabel.Font = new System.Drawing.Font("Calibri Light", 24);
 
                     /* No button properties */
                     RightButton.Height = 30;
                     RightButton.Dock = DockStyle.Bottom;
                     RightButton.Text = question.QuestionAnswers.Item2;
-                    RightButton.Font = new System.Drawing.Font("Calibri Light", 12); ;
+                    RightButton.Font = new System.Drawing.Font("Calibri Light", 12);
 
                     /* Yes button properties */
                     LeftButton.Height = 30;
                     LeftButton.Dock = DockStyle.Bottom;
                     LeftButton.Text = question.QuestionAnswers.Item1;
-                    LeftButton.Font = new System.Drawing.Font("Calibri Light", 12); ;
+                    LeftButton.Font = new System.Drawing.Font("Calibri Light", 12);
+
+                    ControlCollection.Add(LeftButton);
+                    ControlCollection.Add(RightButton);
+                    ControlCollection.Add(QuestionLabel);
                 }
                 public static void Render(Question question)
                 {
                     InitializeComponents(question);
-                    Controls.Add(LeftButton);
-                    Controls.Add(RightButton);
-                    Controls.Add(QuestionLabel);
+                    foreach (var control in ControlCollection)
+                    {
+                        if (!Controls.Contains((Control)control)) Controls.Add((Control)control);
+                    }
+                }
+            }
+            public static class Menu
+            {
+                public static Control.ControlCollection ControlCollection { get; private set; } = new Control.ControlCollection(new Control());
+                public static void InitializeComponents()
+                {
+                    /* Main menu properties */
+                    Components.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                    Components.TestMenuItem,
+                    Components.HelpMenuItem});
+                    Components.MainMenu.Location = new System.Drawing.Point(0, 0);
+                    Components.MainMenu.Name = "MainMenu";
+                    Components.MainMenu.Size = new System.Drawing.Size(FormWidth, 24);
+                    Components.MainMenu.TabIndex = 0;
+                    Components.MainMenu.Text = "menuStrip1";
+
+                    /* Test menu properties */
+                    Components.TestMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                    Components.StartTestMenuItem,
+                    Components.ExitMenuItem});
+                    Components.TestMenuItem.Name = "TestMenuItem";
+                    Components.TestMenuItem.Size = new System.Drawing.Size(42, 20);
+                    Components.TestMenuItem.Text = "Тест";
+
+                    /* Help menu properties */
+                    Components.HelpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                    Components.AboutMenuItem});
+                    Components.HelpMenuItem.Name = "HelpMenuItem";
+                    Components.HelpMenuItem.Size = new System.Drawing.Size(68, 20);
+                    Components.HelpMenuItem.Text = "Помощь";
+
+                    /* Start test menu item */
+                    Components.StartTestMenuItem.Name = "StartTestMenuItem";
+                    Components.StartTestMenuItem.Size = new System.Drawing.Size(180, 22);
+                    Components.StartTestMenuItem.Text = "Начать";
+
+                    /* Exit menu item */
+                    Components.ExitMenuItem.Name = "ExitMenuItem";
+                    Components.ExitMenuItem.Size = new System.Drawing.Size(180, 22);
+                    Components.ExitMenuItem.Text = "Выйти в окно";
+
+                    /* About menu item */
+                    Components.AboutMenuItem.Name = "AboutMenuItem";
+                    Components.AboutMenuItem.Size = new System.Drawing.Size(149, 22);
+                    Components.AboutMenuItem.Text = "О программе";
+
+                    ControlCollection.Add(Components.MainMenu);
+                }
+                public static void Render()
+                {
+                    InitializeComponents();
+                    foreach (var control in ControlCollection)
+                    {
+                        if (!Controls.Contains((Control)control)) Controls.Add((Control)control);
+                    }
                 }
             }
         }
